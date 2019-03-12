d36b560d7ecb9f510be48b2eb19e182e7b7796a7
soheil-khorram <khorram.soheil@gmail.com>
2019-03-12 16:25:02 -0500

    Add files via upload

---
+++ model/conv_net.py
@@ -0,0 +1,83 @@
+from keras.layers import Conv1D, Input, Dropout
+from keras.models import Model, load_model
+from keras.optimizers import Adam
+from keras.regularizers import l2
+from model.keras_net import KerasNet
+
+
+class Net(KerasNet):
+    def __init__(self):
+        super(Net, self).__init__()
+
+    def parse_arguments(self, parser):
+        super(Net, self).parse_arguments(parser)
+        parser.add_argument('-dropout-rate', default=0.0,
+                            type=float, help='randomly setting a fraction of inputs to 0')
+        parser.add_argument('-layer-num', default=10,
+                            type=int, help='number of conv layers')
+        # parser.add_argument('-utt-in-dim', default=40,
+        #                     type=int, help='dimensionality of the input feats')
+        parser.add_argument('-activation', default='relu',
+                            type=str, help='activation type')
+        parser.add_argument('-kernel-num', default=512,
+                            type=int, help='number of the conv filters')
+        parser.add_argument('-kernel-size', default=7,
+                            type=int, help='size of the conv kernels')
+        parser.add_argument('-l2', default=0.0,
+                            type=float, help='l2 regularization factor')
+        # parser.add_argument('-utt-out-dim', default=3393,
+        #                     type=int, help='dimensionality of the output')
+        parser.add_argument('-step-size', default=0.0001,
+                            type=float, help='learning rate')
+        parser.add_argument('-epoch-num', default=20,
+                            type=int, help='number of epochs')
+
+    def set_params(self, prm):
+        super(Net, self).set_params(prm)
+        self.dropout_rate = prm.dropout_rate
+        self.layer_num = prm.layer_num
+        self.utt_in_dim = prm.utt_in_dim
+        self.activation = prm.activation
+        self.kernel_num = prm.kernel_num
+        self.kernel_size = prm.kernel_size
+        self.l2 = prm.l2
+        self.step_size = prm.step_size
+        self.utt_out_dim = prm.utt_out_dim
+        self.epoch_num = prm.epoch_num
+
+    def construct(self):
+        inp = Input(shape=(None, self.utt_in_dim))
+        x = inp
+        for i in range(self.layer_num - 1):
+            x = Conv1D(filters=self.kernel_num,
+                       kernel_size=self.kernel_size,
+                       strides=1,
+                       padding='same',
+                       data_format='channels_last',
+                       dilation_rate=1,
+                       activation=self.activation,
+                       use_bias=True,
+                       kernel_initializer='glorot_uniform',
+                       bias_initializer='zeros',
+                       kernel_regularizer=l2(self.l2),
+                       bias_regularizer=l2(self.l2),
+                       activity_regularizer=None)(x)
+            if self.dropout_rate != 0:
+                x = Dropout(self.dropout_rate)(x)
+        x = Conv1D(filters=self.utt_out_dim,
+                   kernel_size=1,
+                   strides=1,
+                   padding='same',
+                   data_format='channels_last',
+                   dilation_rate=1,
+                   activation='softmax',
+                   use_bias=True,
+                   kernel_initializer='glorot_uniform',
+                   bias_initializer='zeros',
+                   kernel_regularizer=l2(self.l2),
+                   bias_regularizer=l2(self.l2),
+                   activity_regularizer=None)(x)
+        outp = x
+        self._net = Model(inputs=inp, outputs=outp)
+        self._net.compile(optimizer=Adam(lr=self.step_size),
+                          loss='categorical_crossentropy')
---
+++ model/keras_net.py
@@ -0,0 +1,74 @@
+import os
+import numpy as np
+from keras.models import load_model
+import json
+
+
+class KerasNet:
+    def __init__(self):
+        self._net = None
+
+    def parse_arguments(self, parser):
+        base_path = os.path.dirname(os.path.abspath(__file__))
+        exp_path = base_path + '/../../exp_results'
+        parser.add_argument('-net-summary-path', default=exp_path + '/net_summary.txt',
+                            type=str, help='network summary path')
+        # parser.add_argument('-utt_pad_lab', default=3392,
+        #                     type=int, help='use this lable to do the padding')
+
+    def set_params(self, prm):
+        self.net_summary_path = prm.net_summary_path
+        self.utt_pad_lab = prm.utt_pad_lab
+
+    def construct(self):
+        raise NotImplementedError('Implement it.')
+
+    def save_summary(self):
+        cnf_dic = self._net.get_config()
+        text = json.dumps(cnf_dic, indent=4)
+        with open(self.net_summary_path, 'a') as file_id:
+            file_id.write(text + '\n')
+        return
+
+    def train_one_epoch(self, data_generator):
+        self._net.fit_generator(
+            generator=data_generator,
+            epochs=1,
+            steps_per_epoch=len(data_generator.ids))
+
+    def evaluate(self, data_generator, result_handlers=[]):
+        for r in result_handlers:
+            r.start()
+        num_of_chunks = len(data_generator.ids)
+        for c in range(num_of_chunks):
+            X = data_generator.X[c]
+            y_true_cmp = data_generator.y[c]
+            ids = data_generator.ids[c]
+            y_pred = self._net.predict(X)
+            y_pred_cmp = np.argmax(y_pred, -1)
+            for u in range(y_true_cmp.shape[0]):
+                utt_id = ids[u]
+                utt_y_true_cmp = y_true_cmp[u]
+                utt_y_pred_cmp = y_pred_cmp[u]
+                utt_y_pred = y_pred[u]
+                utt_y_pred_cmp = utt_y_pred_cmp[utt_y_true_cmp != self.utt_pad_lab]
+                utt_y_pred = utt_y_pred[utt_y_true_cmp != self.utt_pad_lab, :]
+                utt_y_true_cmp = utt_y_true_cmp[utt_y_true_cmp != self.utt_pad_lab]
+                for r in result_handlers:
+                    r.get_id(utt_id)
+                    r.get_y_true_cmp(utt_y_true_cmp)
+                    r.get_y_pred_cmp(utt_y_pred_cmp)
+                    r.get_y_pred(utt_y_pred)
+                    r.finalize_iter()
+        for r in result_handlers:
+            r.end()
+
+    def save(self, path):
+        full_path = os.path.abspath(path)
+        dir_path = os.path.dirname(full_path)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        self._net.save(full_path)
+
+    def load(self, path):
+        self._net = load_model(path)
