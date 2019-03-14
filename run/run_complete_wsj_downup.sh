6798b1997d5a59e494df88d3b7fc50f665240ca4
soheil-khorram <khorram.soheil@gmail.com>
2019-03-13 21:13:07 -0500

    hourglass is added

--- run/run_complete_wsj_downup.sh
+++ run/run_complete_wsj_downup.sh
@@ -7,30 +7,32 @@ export CUDA_VISIBLE_DEVICE=0
 base_path=$(pwd)
 base_path=$base_path"/../../exp_results"
 mkdir -p $base_path
-hourglass_num=5
-layer_num=(5)
+hourglass_num=(5 3 1)
+layer_num=(5 3)
 kernel_num=512
-kernel_size=(5)
+kernel_size=(5 3)
 epoch_num=20
 step_size=0.0001
 batch_size=8
 
-for ks in "${kernel_size[@]}"; do
-    for ln in ${layer_num[@]}; do
-        out_dir=$base_path/$DATA_LOADER/$MODEL/ks_${ks}_ln_${ln}/
-        command="\
-            python ../main.py \
-                -batch-size $batch_size \
-                -hourglass-num $hourglass_num \
-                -out-dir $out_dir \
-                -net-summary-path $out_dir/net_summary.txt \
-                -layer-num $ln \
-                -kernel-size $ks \
-                -kernel-num  $kernel_num \
-                -epoch-num $epoch_num \
-                -step-size $step_size \
-                -down-up-num $ln"
-        echo -e $command
-        $command
+for hgn in "${hourglass_num[@]}"; do
+    for ks in "${kernel_size[@]}"; do
+        for ln in ${layer_num[@]}; do
+            out_dir=$base_path/$DATA_LOADER/$MODEL/ks_${ks}_ln_${ln}/
+            command="\
+                python ../main.py \
+                    -batch-size $batch_size \
+                    -hourglass-num $hgn \
+                    -out-dir $out_dir \
+                    -net-summary-path $out_dir/net_summary.txt \
+                    -layer-num $ln \
+                    -kernel-size $ks \
+                    -kernel-num  $kernel_num \
+                    -epoch-num $epoch_num \
+                    -step-size $step_size \
+                    -down-up-num $ln"
+            echo -e $command
+            $command
+        done
     done
 done
