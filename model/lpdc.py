a112e0a4b8b89a4bee206e4bda2e21f4384ad39c
soheil-khorram <khorram.soheil@gmail.com>
2019-03-15 00:41:32 -0500

    lpdc was added

---
+++ model/lpdc.py
@@ -0,0 +1,92 @@
+from keras.layers import Conv1D, Input, Add
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.regularizers import l2
+from model.keras_net import KerasNet
+
+
+class Net(KerasNet):
+    def __init__(self):
+        super(Net, self).__init__()
+
+    def parse_arguments(self, parser):
+        super(Net, self).parse_arguments(parser)
+        parser.add_argument('-lpdc-num', default=4, type=int)
+        parser.add_argument('-lpdc-depth', default=4, type=int)
+        parser.add_argument('-activation', default='relu', type=str)
+        parser.add_argument('-kernel-num', default=512, type=int)
+        parser.add_argument('-kernel-size', default=3, type=int)
+        parser.add_argument('-l2', default=0.0, type=float)
+        parser.add_argument('-step-size', default=0.0001,
+                            type=float, help='learning rate')
+        parser.add_argument('-epoch-num', default=20,
+                            type=int, help='number of epochs')
+        # parser.add_argument('-utt-out-dim', default=3393,
+        #                     type=int, help='dimensionality of the output')
+        # parser.add_argument('-utt-in-dim', default=40,
+        #                     type=int, help='dimensionality of the input feats')
+
+    def set_params(self, prm):
+        super(Net, self).set_params(prm)
+        self.lpdc_num = prm.lpdc_num
+        self.lpdc_depth = prm.lpdc_depth
+        self.activation = prm.activation
+        self.kernel_num = prm.kernel_num
+        self.kernel_size = prm.kernel_size
+        self.l2 = prm.l2
+        self.step_size = prm.step_size
+        self.epoch_num = prm.epoch_num
+        self.utt_in_dim = prm.utt_in_dim
+        self.utt_out_dim = prm.utt_out_dim
+
+    def lpdc(self, x):
+        y = None
+        for i in range(self.lpdc_depth):
+            x = self.dilated_conv(x, 2 ** i)
+            if y is None:
+                y = x
+            else:
+                y = Add()([y, x])
+        return y
+
+    def dilated_conv(self, x, dilation_rate):
+        return Conv1D(filters=self.kernel_num,
+                      kernel_size=self.kernel_size,
+                      strides=1,
+                      padding='same',
+                      data_format='channels_last',
+                      dilation_rate=dilation_rate,
+                      activation=self.activation,
+                      use_bias=True,
+                      kernel_initializer='glorot_uniform',
+                      bias_initializer='zeros',
+                      kernel_regularizer=l2(self.l2),
+                      bias_regularizer=l2(self.l2),
+                      activity_regularizer=None)(x)
+
+    def last_conv(self, x):
+        return Conv1D(filters=self.utt_out_dim,
+                      kernel_size=1,
+                      strides=1,
+                      padding='same',
+                      data_format='channels_last',
+                      dilation_rate=1,
+                      activation='softmax',
+                      use_bias=True,
+                      kernel_initializer='glorot_uniform',
+                      bias_initializer='zeros',
+                      kernel_regularizer=l2(self.l2),
+                      bias_regularizer=l2(self.l2),
+                      activity_regularizer=None)(x)
+
+    def construct(self):
+        inp = Input(shape=(None, self.utt_in_dim))
+        x = inp
+        x = self.dilated_conv(x, 1)
+        for i in range(self.lpdc_num):
+            x = self.lpdc(x)
+        x = self.last_conv(x)
+        outp = x
+        self._net = Model(inputs=inp, outputs=outp)
+        self._net.compile(optimizer=Adam(lr=self.step_size),
+                          loss='categorical_crossentropy')
---
+++ run/run_complete_wsj_lpdc.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+export MODEL='model.lpdc'
+export DATA_LOADER='data_loader.wsj_data_loader'
+export CUDA_VISIBLE_DEVICE=0
+
+
+base_path=$(pwd)
+base_path=$base_path"/../../exp_results"
+mkdir -p $base_path
+lpdc_num_ls=(4)
+lpdc_depth_ls=(4)
+kernel_num=512
+kernel_size=3
+step_size=0.0001
+epoch_num=20
+batch_size=16
+
+for lpdc_num in "${lpdc_num_ls[@]}"; do
+    for lpdc_depth in "${lpdc_depth_ls[@]}"; do
+        out_dir=$base_path/$DATA_LOADER/$MODEL/lpdc_num_${lpdc_num}_lpdc_depth_${lpdc_depth}/
+        command="\
+            python ../main.py \
+                -lpdc-num $lpdc_num \
+                -lpdc-depth $lpdc_depth \
+                -kernel-num $kernel_num \
+                -kernel-size $kernel_size \
+                -step-size $step_size \
+                -epoch-num $epoch_num \
+                -batch-size $batch_size
+                -net-summary-path $out_dir/net_summary.txt \
+                -out-dir $out_dir"
+        echo -e $command
+        $command
+    done
+done
+
+
+
+base_path=$(pwd)
+base_path=$base_path"/../../exp_results"
+mkdir -p $base_path
+lpdc_num_ls=(5)
+lpdc_depth_ls=(5)
+kernel_num=512
+kernel_size=3
+step_size=0.0001
+epoch_num=20
+batch_size=8
+
+for lpdc_num in "${lpdc_num_ls[@]}"; do
+    for lpdc_depth in "${lpdc_depth_ls[@]}"; do
+        out_dir=$base_path/$DATA_LOADER/$MODEL/lpdc_num_${lpdc_num}_lpdc_depth_${lpdc_depth}/
+        command="\
+            python ../main.py \
+                -lpdc-num $lpdc_num \
+                -lpdc-depth $lpdc_depth \
+                -kernel-num $kernel_num \
+                -kernel-size $kernel_size \
+                -step-size $step_size \
+                -epoch-num $epoch_num \
+                -batch-size $batch_size
+                -net-summary-path $out_dir/net_summary.txt \
+                -out-dir $out_dir"
+        echo -e $command
+        $command
+    done
+done
+
+
+
+base_path=$(pwd)
+base_path=$base_path"/../../exp_results"
+mkdir -p $base_path
+lpdc_num_ls=(5)
+lpdc_depth_ls=(3)
+kernel_num=512
+kernel_size=3
+step_size=0.0001
+epoch_num=20
+batch_size=16
+
+for lpdc_num in "${lpdc_num_ls[@]}"; do
+    for lpdc_depth in "${lpdc_depth_ls[@]}"; do
+        out_dir=$base_path/$DATA_LOADER/$MODEL/lpdc_num_${lpdc_num}_lpdc_depth_${lpdc_depth}/
+        command="\
+            python ../main.py \
+                -lpdc-num $lpdc_num \
+                -lpdc-depth $lpdc_depth \
+                -kernel-num $kernel_num \
+                -kernel-size $kernel_size \
+                -step-size $step_size \
+                -epoch-num $epoch_num \
+                -batch-size $batch_size
+                -net-summary-path $out_dir/net_summary.txt \
+                -out-dir $out_dir"
+        echo -e $command
+        $command
+    done
+done
+
+base_path=$(pwd)
+base_path=$base_path"/../../exp_results"
+mkdir -p $base_path
+lpdc_num_ls=(3)
+lpdc_depth_ls=(5)
+kernel_num=512
+kernel_size=3
+step_size=0.0001
+epoch_num=20
+batch_size=16
+
+for lpdc_num in "${lpdc_num_ls[@]}"; do
+    for lpdc_depth in "${lpdc_depth_ls[@]}"; do
+        out_dir=$base_path/$DATA_LOADER/$MODEL/lpdc_num_${lpdc_num}_lpdc_depth_${lpdc_depth}/
+        command="\
+            python ../main.py \
+                -lpdc-num $lpdc_num \
+                -lpdc-depth $lpdc_depth \
+                -kernel-num $kernel_num \
+                -kernel-size $kernel_size \
+                -step-size $step_size \
+                -epoch-num $epoch_num \
+                -batch-size $batch_size
+                -net-summary-path $out_dir/net_summary.txt \
+                -out-dir $out_dir"
+        echo -e $command
+        $command
+    done
+done
+
